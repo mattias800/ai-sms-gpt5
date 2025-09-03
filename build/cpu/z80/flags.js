0aad9adcdded975df9c1ecb88c90ee747de6f901
Mattias Andersson <mattias800@gmail.com>
2025-09-03 21:42:08 +0200

    chore: remove dist and build folders from git tracking

--- build/cpu/z80/flags.js
+++
@@ -1,8 +0,0 @@
-export const FLAG_S = 0x80;
-export const FLAG_Z = 0x40;
-export const FLAG_5 = 0x20;
-export const FLAG_H = 0x10;
-export const FLAG_3 = 0x08;
-export const FLAG_PV = 0x04;
-export const FLAG_N = 0x02;
-export const FLAG_C = 0x01;
--- dist/tests/setup.js
+++
@@ -1,5 +0,0 @@
-import { configureGlobal } from 'fast-check';
-const seedStr = process.env.TEST_SEED ?? '1337';
-const seed = Number.isFinite(Number(seedStr)) ? Number(seedStr) : 1337;
-configureGlobal({ seed, numRuns: 200 });
-//# sourceMappingURL=setup.js.map
